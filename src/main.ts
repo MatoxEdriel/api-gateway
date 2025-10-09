c4cb26b21293b3cc26afaf396dc6e16e23b6644d
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-09 09:49:31 -0500

    prueba

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { envs } from './config/envs';
 async function bootstrap() {
   const logger = new Logger('Main-gateway');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 
   logger.log(`Gateway corriendo en el puerto ${envs.port}`);
-  logger.log(`invoice host  ${envs.invoiceHost}`);
+  logger.log(`invoice host que seria localhost  ${envs.invoiceHost}`);
 }
 
 bootstrap();
