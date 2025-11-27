8978bc570b9afa2082a163b85e4197f34c38f413
MatoxEdriel <ga99_buggi@hotmail.com>
2025-11-26 22:50:53 -0500

    Kafka implementacion apigateway -- revisar concpetos de puertos y conexiones

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,44 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './module/products/products.module';
 import { InvoiceModule } from './module/invoice/invoice.module';
 import { CustomersModule } from './module/customers/customers.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi'
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 //Aqui configuras el configService 
 @Module({
   imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_CLIENT',
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: configService.get<string>('KAFKA_CLIENT_ID'),
+              brokers: [configService.get<string>('KAFKA_BROKERS')?.split(',')?.toString() || 'localhost:9092'],
+            },
+            consumer: {
+              groupId: 'api-gateway-group',
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+
+
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
       envFilePath: '.env',
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
         PLANTS_MICROSERVICES_HOST: Joi.string().required(),
-        PLANTS_MICROSERVICES_PORT: Joi.number().required()
+        PLANTS_MICROSERVICES_PORT: Joi.number().required(),
+
+        KAFKA_BROKERS: Joi.string().required(),
+        KAFKA_CLIENT_ID: Joi.string().required(),
 
       })
 
